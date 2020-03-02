32fac2db48052c0f057bfb8b53608eaa6b2ceed5
Andreas Rissling <andreas.rissling@gmx.net>
2020-03-02 16:29:54 +0100

    Adjust prop-tyoe for pad component

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components/macro'
 
 Pad.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string.isRequired,
 }
 
 export default function Pad({ name }) {
