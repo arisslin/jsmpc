fc68d6105ed770dc63052f180c3a528faec5c232
Andreas Rissling <andreas.rissling@gmx.net>
2020-03-02 15:23:04 +0100

    Lable pads

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -2,12 +2,24 @@ import React from 'react'
 import styled from 'styled-components/macro'
 
 export default function Pad({ name }) {
-  return <PadStyled>{name}</PadStyled>
+  return (
+    <PadStyled>
+      <span>{name}</span>
+    </PadStyled>
+  )
 }
 
 const PadStyled = styled.div`
+  position: relative;
   border: 2px solid var(--color-dark);
   border-radius: 3px;
   background-color: var(--color-pad-grey);
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.2);
+
+  span {
+    position: absolute;
+    top: -0.7rem;
+    font-size: 0.5rem;
+    color: #3d4448;
+  }
 `
--- src/components/PadSection.js
+++ src/components/PadSection.js
@@ -3,9 +3,9 @@ import styled from 'styled-components/macro'
 import Pad from './Pad'
 
 export default function PadSection({ pads }) {
-  return <PadSectionStyled>{pads.map(pad => addPadd(pad))}</PadSectionStyled>
+  return <PadSectionStyled>{pads.map(pad => addPad(pad))}</PadSectionStyled>
 
-  function addPadd(pad) {
+  function addPad(pad) {
     return <Pad key={pad.name} name={pad.name} />
   }
 }
@@ -15,8 +15,8 @@ const PadSectionStyled = styled.section`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr 1fr;
-  grid-gap: 10px;
-  padding: 10px;
+  grid-gap: 12px;
+  padding: 12px;
   border: 2px solid var(--blue-grey-medium);
   border-radius: 3px;
   background-color: var(--blue-grey-light);
