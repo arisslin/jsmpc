39570df395d984cb92a628ea2437fa9ac9049c65
Andreas Rissling <andreas.rissling@gmx.net>
2020-03-04 18:11:08 +0100

    Put pads array in own js file and added drumkit sound url

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React from 'react'
 import styled from 'styled-components/macro'
+import { pads } from './assets/pads'
 import PadSection from './components/PadSection'
 
 export default function App() {
-  const pads = [
-    // Ordered like the pads in real mpc 2000 xl
-    { name: 'Pad 13' },
-    { name: 'Pad 14' },
-    { name: 'Pad 15' },
-    { name: 'Pad 16' },
-    { name: 'Pad 9' },
-    { name: 'Pad 10' },
-    { name: 'Pad 11' },
-    { name: 'Pad 12' },
-    { name: 'Pad 5' },
-    { name: 'Pad 6' },
-    { name: 'Pad 7' },
-    { name: 'Pad 8' },
-    { name: 'Pad 1' },
-    { name: 'Pad 2' },
-    { name: 'Pad 3' },
-    { name: 'Pad 4' },
-  ]
-
   return (
     <AppStyled>
       <PadSection pads={pads} />
---
+++ src/assets/pads.js
@@ -0,0 +1,19 @@
+export const pads = [
+  // Ordered like the pads in real mpc 2000 xl
+  { name: 'Pad 13', url: './media/sounds/808kit/OpenHH808.wav' },
+  { name: 'Pad 14', url: './media/sounds/808kit/Rimshot808.wav' },
+  { name: 'Pad 15', url: './media/sounds/808kit/Clave808.wav' },
+  { name: 'Pad 16', url: './media/sounds/808kit/Cowbell808.wav' },
+  { name: 'Pad 9', url: './media/sounds/808kit/LowConga808.wav' },
+  { name: 'Pad 10', url: './media/sounds/808kit/HiConga808.wav' },
+  { name: 'Pad 11', url: './media/sounds/808kit/LowTom808.wav' },
+  { name: 'Pad 12', url: './media/sounds/808kit/HiTom808.wav' },
+  { name: 'Pad 5', url: './media/sounds/808kit/Kick808_2.wav' },
+  { name: 'Pad 6', url: './media/sounds/808kit/Snare808_2.wav' },
+  { name: 'Pad 7', url: './media/sounds/808kit/Shaker808.wav' },
+  { name: 'Pad 8', url: './media/sounds/808kit/Crash808.wav' },
+  { name: 'Pad 1', url: './media/sounds/808kit/Kick808_1.wav' },
+  { name: 'Pad 2', url: './media/sounds/808kit/Snare808_1.wav' },
+  { name: 'Pad 3', url: './media/sounds/808kit/ClosedHH808.wav' },
+  { name: 'Pad 4', url: './media/sounds/808kit/Clap808.wav' },
+]
