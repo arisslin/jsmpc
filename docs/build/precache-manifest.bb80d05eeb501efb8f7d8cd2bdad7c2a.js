7be7bc5b4a5575d8b4f2046cad4a2c7658fada16
Andreas Rissling <36743463+arisslin@users.noreply.github.com>
2020-03-05 20:04:51 +0100

    Add files via upload

---
+++ docs/build/precache-manifest.bb80d05eeb501efb8f7d8cd2bdad7c2a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73d80242e3b5a10d2d027c322572473",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd7049a5bd32b83ebcaa",
+    "url": "/static/js/2.b57cf342.chunk.js"
+  },
+  {
+    "revision": "405ac93e2042eacdc1cf3d9ff9b7c648",
+    "url": "/static/js/2.b57cf342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86000a2f224ea2fdbee",
+    "url": "/static/js/main.371854bf.chunk.js"
+  },
+  {
+    "revision": "a42fe5d103d78e9fbb09",
+    "url": "/static/js/runtime-main.432f846a.js"
+  },
+  {
+    "revision": "848dbc3f858589ae2a197c026d52084d",
+    "url": "/static/media/Clap808.848dbc3f.wav"
+  },
+  {
+    "revision": "57a079e555470f90c1a52d2be7d0c725",
+    "url": "/static/media/Clave808.57a079e5.wav"
+  },
+  {
+    "revision": "7d30054fdf113eb575d577f51271468e",
+    "url": "/static/media/ClosedHH808.7d30054f.wav"
+  },
+  {
+    "revision": "144a4c6dc20f16d55687d364c66b60a9",
+    "url": "/static/media/Cowbell808.144a4c6d.wav"
+  },
+  {
+    "revision": "039b2daf6f64fd0e37d8bd0e5fb72e84",
+    "url": "/static/media/Crash808.039b2daf.wav"
+  },
+  {
+    "revision": "5bcca1609486c6eb846b17a8e17e78fc",
+    "url": "/static/media/HiConga808.5bcca160.wav"
+  },
+  {
+    "revision": "3b19b79cb9d5ded9851d34b8d486fa63",
+    "url": "/static/media/HiTom808.3b19b79c.wav"
+  },
+  {
+    "revision": "e9505dc877f439d595b6d3c661cde3d5",
+    "url": "/static/media/Kick808_1.e9505dc8.wav"
+  },
+  {
+    "revision": "9766e751faf98448b98863fdbeccf984",
+    "url": "/static/media/Kick808_2.9766e751.wav"
+  },
+  {
+    "revision": "a9a6879022431970327b5c538ec01361",
+    "url": "/static/media/LowConga808.a9a68790.wav"
+  },
+  {
+    "revision": "5cfb29ce2340e83f2fbccf70fda6248b",
+    "url": "/static/media/LowTom808.5cfb29ce.wav"
+  },
+  {
+    "revision": "a46a6e9af99ff3b7d47cf576be71c930",
+    "url": "/static/media/OpenHH808.a46a6e9a.wav"
+  },
+  {
+    "revision": "b5892d28ae485b2ac894881db2c5b3ef",
+    "url": "/static/media/Rimshot808.b5892d28.wav"
+  },
+  {
+    "revision": "f2b8fd67d72cea8ce2cc6ca6a3aeecaa",
+    "url": "/static/media/Shaker808.f2b8fd67.wav"
+  },
+  {
+    "revision": "398b69edeeaf7e4fc4d4548f22a9915d",
+    "url": "/static/media/Snare808_1.398b69ed.wav"
+  },
+  {
+    "revision": "13b891cbbae10a595773362bea42f417",
+    "url": "/static/media/Snare808_2.13b891cb.wav"
+  }
+]);
