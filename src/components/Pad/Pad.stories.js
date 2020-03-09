50b827f4a6c0cf91fc05ce2f909755c6ca9c9f61
Andreas Rissling <andreas.rissling@gmx.net>
2020-03-09 15:07:28 +0100

    Add action to pad story

---
+++ src/components/Pad/Pad.stories.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import Pad from './Pad'
+import GlobalStyles from '../../common/GlobalStyles'
+
+export default {
+  title: 'Pad',
+  component: Pad,
+}
+
+export const DefaultPad = () => (
+  <div
+    style={{
+      margin: 20,
+      height: '100px',
+      width: '100px',
+      display: 'grid',
+    }}
+  >
+    <GlobalStyles />
+    <Pad name="Pad 1" player={{ start: action('player.start()') }} />
+  </div>
+)
