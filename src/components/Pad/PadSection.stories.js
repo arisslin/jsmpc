593c4478c4465d118f2aaf8fc63f0abe4cd899c5
Andreas Rissling <andreas.rissling@gmx.net>
2020-03-17 09:31:58 +0100

    Adjust storybook for PadSection component

--- src/components/Pad/PadSection.stories.js
+++ src/components/Pad/PadSection.stories.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PadSection from './PadSection'
 import GlobalStyles from '../../common/GlobalStyles'
 import { padsData } from '../../common/padsData'
-import { padPlayer } from '../../common/padPlayer'
 
 export default {
   title: 'Pads',
@@ -12,14 +11,13 @@ export default {
 export const DefaultPadSection = () => (
   <div
     style={{
-      display: 'flex',
-      justifyContent: 'center',
+      display: 'grid',
+      width: 400,
+      height: 400,
       padding: 10,
-      height: '375px',
-      width: '375px',
     }}
   >
     <GlobalStyles />
-    <PadSection pads={padsData} padPlayer={padPlayer} />
+    <PadSection pads={padsData} />
   </div>
 )
