994d4cc005187d679cb7df4e73950a3bb4975f41
Andreas Rissling <andreas.rissling@gmx.net>
2020-03-19 10:11:11 +0100

    Refactor Keyboard component

--- src/components/DisplayPages/DisplayPageInfo.js
+++ src/components/DisplayPages/DisplayPageInfo.js
@@ -7,36 +7,32 @@ export default function DisplayPageInfo() {
   return (
     <PageStyled>
       <DisplayPageTitle title="Keyboard shortcuts" />
-      <section className="keyboard">
+      <Keyboard>
         {padsData.map(pad => (
-          <Key key={pad.key} char={pad.key} />
+          <Key key={pad.key}>{pad.key}</Key>
         ))}
-      </section>
+      </Keyboard>
     </PageStyled>
   )
-}
-
-function Key({ char }) {
-  return <KeyStyled>{char}</KeyStyled>
 }
 
 const PageStyled = styled.section`
   display: flex;
   flex-direction: column;
   height: 100%;
-
-  .keyboard {
-    align-self: center;
-    display: grid;
-    height: 160px;
-    width: 160px;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr;
-    gap: 4px;
-  }
 `
 
-const KeyStyled = styled.div`
+const Keyboard = styled.section`
+  align-self: center;
+  display: grid;
+  height: 160px;
+  width: 160px;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  gap: 4px;
+`
+
+const Key = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
