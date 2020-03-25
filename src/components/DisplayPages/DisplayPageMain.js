1477edfa3ded55935a4e6657aa088339e30019fa
Andreas Rissling <andreas.rissling@gmx.net>
2020-03-25 12:17:19 +0100

    Create Slider component

--- src/components/DisplayPages/DisplayPageMain.js
+++ src/components/DisplayPages/DisplayPageMain.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import DisplayPageTitle from './DisplayPageTitle'
 import styled from 'styled-components/macro'
+import Slider from '../../components/Slider'
 
 export default function DisplayPageMain({ masterVolume, onChangeInputVolume }) {
   return (
     <DisplayPageMainStyled>
       <DisplayPageTitle title="Main" />
-      <Slider htmlFor="volume-slider">
-        Master Volume: <span>{masterVolume}dB</span>
-        <input
-          name="volume-slider"
-          type="range"
-          min="-80"
-          max="6"
-          defaultValue={masterVolume}
-          onChange={onChangeInputVolume}
-        />
-      </Slider>
+      <Slider
+        masterVolume={masterVolume}
+        onChangeInputVolume={onChangeInputVolume}
+      />
     </DisplayPageMainStyled>
   )
 }
@@ -24,107 +18,4 @@ export default function DisplayPageMain({ masterVolume, onChangeInputVolume }) {
 const DisplayPageMainStyled = styled.article`
   display: flex;
   flex-direction: column;
-`
-
-const Slider = styled.label`
-  display: block;
-
-  span {
-    display: inline-block;
-    width: 58px;
-    text-align: right;
-  }
-
-  input[type='range'] {
-    display: block;
-    -webkit-appearance: none;
-    width: 100%;
-    margin: 16px 0;
-    background: transparent;
-  }
-  input[type='range']:focus {
-    outline: none;
-  }
-
-  /* for -webkit */
-
-  input[type='range']::-webkit-slider-runnable-track {
-    width: 100%;
-    height: 8px;
-    cursor: pointer;
-    background: var(--color-display-font-trans-light);
-  }
-
-  input[type='range']::-webkit-slider-thumb {
-    height: 20px;
-    width: 16px;
-    border-radius: 3px;
-    background: var(--color-display-font);
-    cursor: pointer;
-    -webkit-appearance: none;
-    margin-top: -6px;
-  }
-
-  input[type='range']:focus::-webkit-slider-runnable-track {
-    background: var(--color-display-font-trans-light);
-  }
-
-  /* for -moz */
-
-  input[type='range']::-moz-range-track {
-    width: 100%;
-    height: 8px;
-    cursor: pointer;
-    background: var(--color-display-font-trans-light);
-  }
-
-  input[type='range']::-moz-range-thumb {
-    height: 20px;
-    width: 16px;
-    border-radius: 3px;
-    border: none;
-    background: var(--color-display-font);
-    cursor: pointer;
-  }
-
-  /* for -ms */
-
-  input[type='range']::-ms-track {
-    width: 100%;
-    height: 8px;
-    cursor: pointer;
-    background: transparent;
-    border-color: transparent;
-    color: transparent;
-  }
-
-  input[type='range']::-ms-fill-lower {
-    background: var(--color-display-font-trans-light);
-    border: none;
-    border-radius: 0px;
-  }
-
-  input[type='range']::-ms-fill-upper {
-    background: var(--color-display-font-trans-light);
-    border: none;
-    border-radius: 0px;
-  }
-
-  input[type='range']::-ms-thumb {
-    border: none;
-    height: 20px;
-    width: 16px;
-    border-radius: 3px;
-    background: var(--color-display-font);
-    cursor: pointer;
-    height: 8px;
-  }
-
-  input[type='range']:focus::-ms-fill-lower {
-    background: var(--color-display-font-trans-light);
-  }
-
-  input[type='range']:focus::-ms-fill-upper {
-    background: var(--color-display-font-trans-light);
-  }
 `
