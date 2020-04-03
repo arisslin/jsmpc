b0f980f5389dbe94c7ff6652e9ab68c7b669cc93
Andreas Rissling <andreas.rissling@gmx.net>
2020-04-03 13:42:17 +0200

    Reorganize utils

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,21 +1,16 @@
-export function toLowerNoWhiteSpace(string) {
-  string = String(string)
-  return string.toLowerCase().replace(' ', '')
-}
-
-export const updateInArray = (array, data, index) => {
-  return [...array.slice(0, index), data, ...array.slice(index + 1)]
-}
-
 export const focusElementAfterLoad = element => {
   window.addEventListener('load', () => {
     element.current.focus()
   })
 }
 
-export function stopPinchZooming(event) {
-  // Stops the browser zooming when double tab on mobile devices
-  event.preventDefault()
+export function getKeyByName(pads, name) {
+  return pads.reduce((acc, cur) => {
+    if (name === toLowerNoWhiteSpace(cur.name)) {
+      acc = acc + cur.key
+    }
+    return acc
+  }, '')
 }
 
 export function getPadNameByKey(pads, key) {
@@ -27,11 +22,16 @@ export function getPadNameByKey(pads, key) {
   }, '')
 }
 
-export function getKeyByName(pads, name) {
-  return pads.reduce((acc, cur) => {
-    if (name === toLowerNoWhiteSpace(cur.name)) {
-      acc = acc + cur.key
-    }
-    return acc
-  }, '')
+export function stopPinchZooming(event) {
+  // Stops the browser zooming when double tab on mobile devices
+  event.preventDefault()
+}
+
+export function toLowerNoWhiteSpace(string) {
+  string = String(string)
+  return string.toLowerCase().replace(' ', '')
+}
+
+export const updateInArray = (array, data, index) => {
+  return [...array.slice(0, index), data, ...array.slice(index + 1)]
 }
